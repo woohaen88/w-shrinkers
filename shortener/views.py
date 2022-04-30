7a788a06dc787699440bae7b34a32b6c110692a3
woohaen88 <woohaen88@gmail.com>
2022-05-01 07:52:50 +0900

    request index 작성

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from shortener.models import Users as UserModel
 
 # Create your views here.
+def index(request):
+    user = UserModel.objects.filter(username="admin").first()
+    email = user.email if user else "Anonymous Email!"
+
+    if not request.user.is_authenticated:
+        email = "Anonymous Email!"
+
+    context = {
+        "welcome_msg" : "hi hi hi hi hi hi hi hi hi hi",
+        "email" : email
+    }
+    return render(request, 'index.html', context=context)
