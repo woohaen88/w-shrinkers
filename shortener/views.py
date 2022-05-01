abd26096687b722e92a470070d82991e7896ae79
woohaen88 <woohaen88@gmail.com>
2022-05-01 15:17:31 +0900

    디자인추가2

--- shortener/views.py
+++ shortener/views.py
@@ -3,21 +3,24 @@ from shortener.forms import SigninForm, SignupForm
 from shortener.models import Users as UserModel
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    # # print(request.user.pay_plan.name)
-    # user = UserModel.objects.filter(username=request.user.email).first()
-    # email = user.email if user else "Anonymous Email!"
+    user = UserModel.objects.filter(id=request.user.id).first()
+    email = user.email if user else "Anonymous Email!"
 
-    # if not request.user.is_authenticated:
-    #     email = "Anonymous Email!"
+    if not request.user.is_authenticated:
+        email = "Anonymous Email!"
 
     context = {
-        "welcome_msg" : "hi hi hi hi hi hi hi hi hi hi",
-        
+        "welcome_msg" : "hi hi hi hi hi hi hi hi hi hi",        
     }
     return render(request, 'index.html', context=context)
+
+
+def url_list(request):
+    return render(request, "url_list.html")
 
 
 
@@ -25,12 +28,14 @@ def index(request):
 def signup(request):
     if request.method == "POST":
         form = SignupForm(request.POST)
+        msg = "올바르지 않은 데이터입니다."
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
             raw_password =form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            msg = "회원가입완료"
             return redirect('index')                
     else:
         form = SignupForm()
@@ -40,25 +45,45 @@ def signup(request):
 
 # signin
 def signin(request):
+    is_ok = False
     if request.method == "POST":
         form = SigninForm(request.POST)
         if form.is_valid():            
             email = form.cleaned_data.get("email")
-            print(email)
-            user = UserModel.objects.get(email=email)
             raw_password = form.cleaned_data.get("password")
-            if user.check_password(raw_password):
-                login(request, user)
-                return redirect('index')
+            remember_me = form.cleaned_data.get("remember_me")
+            msg = "올바른 유저 ID와 패스워들 입력하시오"
+            try:
+                user = UserModel.objects.get(email=email)
+            except Exception:
+                pass
+            else:
+                if user.check_password(raw_password):
+                    msg = None
+                    login(request, user)
+                    is_ok = True
+                    request.session["remember_me"] = remember_me  
         else:
             messages.error(request, "아이디나 비번 틀림")
 
     else:
         form = SigninForm()
 
-    context = {"form" : form}
+    context = {"form" : form,
+               "msg" : msg,
+               "is_ok" : is_ok}
     return render(request, "signin.html", context=context)
 
 def signout(request):
     logout(request)
     return redirect('signin')
+
+
+@login_required
+def list_view(request):
+    page = int(request.GET.get("p", 1))
+    users = UserModel.objects.all().order_by("-id")
+    paginator = Paginator(users, 10)
+    users = paginator.get_page(page)
+
+    return render(request, "boards.html", {"users": users})
--- shrinkers/settings.py
+++ shrinkers/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = "/login"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]  # Django Debug Toolbar
 
 # Application definition
 
@@ -107,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
